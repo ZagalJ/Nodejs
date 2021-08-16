94b0b4fd95e892992a065b4b0e4bb2d51a7f511c
ZagalJ <jonathan.zagal@hotmail.com>
2021-08-15 21:15:22 -0500

    installed inquirer as well as some basic code for testing

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
 console.log("** Start of New Homework **")
+
+inquirer
+    .prompt([
+        {
+            type:"input",
+            message:"What's the name of your project?",
+            name:"title"
+        },
+        {
+            type:"input",
+            message:"Provide a short explanation of your code. ",
+            name:"explanation"
+        },
+        {
+            type:"confirm",
+            message:"Would you like to have an table of contents?",
+            name:"index"
+        }
+    ])
+    .then((response) => 
+        console.log("Data saved. Wait for ReadMe File")
+    );
