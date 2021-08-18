46f2e312ffab06629f921d077db012c8b8ac5a06
ZagalJ <jonathan.zagal@hotmail.com>
2021-08-18 00:54:21 -0500

    deleted unnecesary code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { async } = require('rxjs');
 
 
 // TODO: Create an array of questions for user input
