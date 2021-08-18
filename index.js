54806d2fae6bdbe67c1384560e35c9c56bfd292d
ZagalJ <jonathan.zagal@hotmail.com>
2021-08-17 21:14:53 -0500

    added starter code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
+// TODO: Create an array of questions for user input
+const questions = [];
 console.log("** Start of New Homework **")
 
 inquirer
@@ -25,3 +27,17 @@ inquirer
     .then((response) => 
         console.log("Data saved. Wait for ReadMe File")
     );
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+
+
+
